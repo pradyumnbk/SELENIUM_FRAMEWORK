243feb3f0c94941294b23e9fe23c5116d938fa5f
pradyumnbk <barik.pradyumn0@gmail.com>
2021-08-08 00:10:43 +0530

    second commit

---
+++ SDET18-SELENIUM/src/test/java/com/crm/sdet18selenium/DemoTest.java
@@ -0,0 +1,12 @@
+package com.crm.sdet18selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void demoTest()
+	{
+		System.out.println("hey i am in selenium framework package");
+	}
+}
